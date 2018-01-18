d6abc65a48a5f277c05654f6243b1b38909bfaa3
Yamaguchi <17.punch@gmail.com>
2018-01-19 00:06:44 +0900

    first commit

---
+++ genesis.py
@@ -0,0 +1,12 @@
+from block import Block
+import datetime as date
+
+
+def create_genesis_block():
+    """
+    manually creates a first block of a blockchain with index of 0 and
+    arbitrary previous hash.
+    first_block = create_genesis_block()
+    first_block
+    """
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
